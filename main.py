9d4d188ed9c1594f52b129e292eb79dc02a98f8b
EdLeafe <ed@leafe.com>
2017-03-26 16:48:18 -0500

    Updated names

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import zzz
-print "c1", dir(zzz.bb.cc.c1)
-print "c2", dir(zzz.bb.cc.c2)
-print "c3", dir(zzz.bb.cc.c3)
+import os_traits
+print "c1", dir(os_traits.bb.cc.c1)
+print "c2", dir(os_traits.bb.cc.c2)
+print "c3", dir(os_traits.bb.cc.c3)
